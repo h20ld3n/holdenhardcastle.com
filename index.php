707398d17fe2da0afb0eb60447d3801e2046e6ca
Holden Hardcastle <hhardcastle@aee.net>
2015-06-30 08:10:13 -0700

    update css
    figured out image size

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<title>Holden Hardcastle</title>
+
+<!-- Bootstrap -->
+<link rel="stylesheet" type="text/css" href="bootstrap/dist/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/golden-ratio.css">
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<div class="container">
+  <h1>Holden Hardcastle (1.61803 + Bootstrap v3.3.5)</h1>
+  <div class="row">
+    <div class="col-md-12"><img src="images/5120.png" class="img-responsive"></div>
+  </div>
+  <div class="row">&nbsp;</div>
+  <div class="row">
+    <div class="col-md-7"><img src="images/5120.png" class="img-responsive"></div>
+    <div class="col-md-5">5</div>
+  </div>
+  <div class="row">&nbsp;</div>
+  <div class="row">
+    <div class="col-md-5"><img src="images/5120.png" class="img-responsive"></div>
+    <div class="col-md-7">7</div>
+  </div>
+  <div class="row">&nbsp;</div>
+  <div class="row">
+    <div class="col-md-3"><img src="images/5120.png" class="img-responsive"></div>
+    <div class="col-md-9">9</div>
+  </div>
+  <div class="row">&nbsp;</div>
+<div class="row">
+    <div class="col-md-2"><img src="images/5120.png" class="img-responsive"></div>
+    <div class="col-md-10">10</div>
+  </div>
+  <div class="row">&nbsp;</div>  
+  <div class="row">
+    <div class="col-md-1"><img src="images/5120.png" class="img-responsive"></div>
+    <div class="col-md-11">11</div>
+  </div>
+  <div class="row">&nbsp;</div>  
+</div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
