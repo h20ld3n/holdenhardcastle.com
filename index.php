442c9f8b1df0a8c08f747e38e9e90d6d1b0e5772
Holden Hardcastle <hhardcastle@aee.net>
2015-12-10 22:46:53 -0800

    Update Carousel

--- index.php
+++ index.php
@@ -57,33 +57,64 @@ function() {
     </section>
 </div>
 <section class="container">
+   <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="images/aee-covers.jpg" alt="...">
+    </div>
+    <div class="item">
+      <img src="images/aee-ohio-electricity-future.jpg" alt="...">
+    </div>
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</section>
+
+
+<section class="container">
 	<!-- advanced energy economy -->
     <!-- report covers -->
 	<div class="row">
   		<div class="col-md-12">
     		<img src="images/aee-covers.jpg" class="img-responsive">
-  			<h2 id="what"></h2>
+  			<h3 id="what"></h3>
   		</div>
 	</div>
     <!-- ohio electricity future -->
 	<div class="row">
   		<div class="col-md-12">
     		<img src="images/aee-ohio-electricity-future.jpg" class="img-responsive">
-  			<h2 id="what"></h2>
-  		</div>
+        <h3 id="what"></h3>
 	</div>
     <!-- infographics -->
 	<div class="row">
   		<div class="col-md-12">
     		<img src="images/aee-infographics.jpg" class="img-responsive">
-  			<h2 id="what"></h2>
+  			<h3 id="what"></h3>
   		</div>
 	</div>
     <!-- letterhead -->
 	<div class="row">
   		<div class="col-md-12">
     		<img src="images/aee-letterhead.jpg" class="img-responsive">
-  			<h2 id="what"></h2>
+  			<h3 id="what"></h3>
   		</div>
 	</div>
 </section>
@@ -92,7 +123,6 @@ function() {
 	<div class="row">
   		<div class="col-md-12">
     		<img src="images/gap-old-navy-email.jpg" class="img-responsive">
-  			<h2 id="what"></h2>
   		</div>
 	</div>
 </section>
@@ -100,9 +130,8 @@ function() {
 	<!-- dictionary.com -->
 	<div class="row">
   		<div class="col-md-12">
-    		<img src="images/dictionary.jpg" class="img-responsive">
-  			<h2 id="what"></h2>
-  		</div>
+        <img src="images/dictionary.jpg" class="img-responsive" alt="dictionary.com" >
+        </div>
 	</div>
 </section>
 <section class="container">
@@ -424,6 +453,7 @@ function() {
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
 <!-- Include all compiled plugins (below), or include individual files as needed --> 
-<script src="js/bootstrap.min.js"></script>
+<!-- script src="js/bootstrap.min.js"></script-->
+<script src="bootstrap/js/carousel.js"></script>
 </body>
 </html>
