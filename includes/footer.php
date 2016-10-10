ef8daadb24ee9b20d0c12aeee165312b033cadae
Holden Hardcastle <hhardcastle@aee.net>
2016-10-10 14:33:01 -0700

    Footer creation

---
+++ includes/footer.php
@@ -0,0 +1,136 @@
+<footer>
+		<div class="jumbotron">
+			<div class="container-fluid">
+  				<div class="row text-muted">
+    				<div class="col-md-3">
+                    	<p>
+                        	<h3>About H2</h3>
+                 			Throughout my professional career I have collaborated with teams at Apple, Amazon, Sony, HBO, and Lucasfilm. I've worked with authors, editors, and artists to push creative boundaries. I thrive on generously sharing ideas, resources, and information with my peers. Whether it's bringing people together through an application or developing a dynamic brand to change the world, I believe that inspired teams yield inspirational results.      						<h3>Education</h3>
+      						<dl class="h4">Griffith University</dl>
+                            	<dt>Master of Arts</dt>
+                                <dd>Media Production with Honours</dd>
+	      					<dl class="h4">Old Dominion University</dl>
+    	  						<dt>Bachelor of Fine Arts</dt>
+                                <dd>Graphic Design Cum Laude</dd>
+	      					<h3>Knowledge</h3>
+    	  					<ul>
+                            	<li>Adobe Creative Suite</li>
+                                <li>Front-end web development - HTML5, CSS3, and JavaScript</li>
+                                <li>Print and Video production</li>
+                          	</ul>
+                            <h3>Contact<!--, Downloads--> &amp; Other Sites</h3>
+                            <ul>
+                            	<li><a href="mailto:h0ld3n@gmail.com"><span class="social social-e-mail"></span>h0ld3n@gmail.com</a></li>
+                                <!-- li><a href="https://about.me/holdenhardcastle" target="_blank"><span class="social social-rss"></span>about me</a></li -->
+                                <!-- li><a href="https://www.behance.net/h20ld3n" target="_blank"><span class="social social-behance"></span>behance</a></li -->
+                                <!-- li><a href="http://cargocollective.com/holdenhardcastle" target="_blank"><span class="social social-rss"></span>cargo collective</a></li -->
+                                <!-- li><a href="https://dribbble.com/h20ld3n" target="_blank"><span class="social social-dribbble"></span>dribbble</a></li -->
+                                <!-- li><a href="http://holdenhardcastle.dunked.com/" target="_blank"><span class="social social-rss"></span>dunked</a></li -->
+                                <!-- li><a href="https://ello.co/h0ld3n" target="_blank"><span class="social social-rss"></span>ello</a></li -->
+                                <li><a href="https://www.facebook.com/holden.hardcastle" target="_blank"><span class="social social-facebook"></span>facebook</a></li>
+                                <li><a href="https://github.com/h20ld3n" target="_blank"><span class="social social-github"></span>github</a></li>
+                                <!-- li><a href="https://plus.google.com/101862561720087132645/posts/p/pub" target="_blank"><span class="social social-google-plus"></span>google+</a></li -->
+                                
+                                <li><a href="https://www.instagram.com/h20ld3n/" target="_blank"><span class="social social-instagram"></span>instagram</a></li>
+                                <li><a href="https://www.linkedin.com/in/holdenhardcastle" target="_blank"><span class="social social-linked-in"></span>linkedin</a></li>
+                                <!-- li><a href="https://localsolo.com/san-francisco-ca/profile/16124/" target="_blank"><span class="social social-rss"></span>localsolo</a></li -->
+                                 <!-- li><a href="https://medium.com/@hhardcastle" target="_blank"><span class="social social-rss"></span>medium</a></li -->
+                                <li><a href="https://www.pinterest.com/h20ld3n/" target="_blank"><span class="social social-pinterest"></span>pinterest</a></li>
+                                <li><a href="pdf/holden-hardcastle.pdf" target="_blank"><span class="social social-rss"></span>resume</a></li>
+                                <!-- li><a href="http://www.shocase.com/go/user/rg50gm8kijioS1kys6udx99kii" target="_blank"><span class="social social-rss"></span>showcase</a></li -->
+                                <li><a href="https://www.tumblr.com/blog/h0ld3n" target="_blank"><span class="social social-tumblr"></span>tumblr</a></li>
+                                <!-- li><a href="http://tevko.github.io/practice/" target="_blank"><span class="social social-rss"></span>tevko - Practice your skills by getting random suggestions or creative inspiration!</a></li-->
+                                <li><a href="https://twitter.com/h20ld3n" target="_blank"><span class="social social-twitter"></span>twitter</a></li>
+                                
+                                <!-- Resumes Additional Portfolios -->
+                                <!--li><a href="https://angel.co/holden-hardcastle" target="_blank"><span class="social social-rss"></span>angel.co</a></li-->
+                                <!--li><a href="https://hired.com/profile.holden-hardcastle" target="_blank"><span class="social social-rss"></span>hired</a></li-->
+                                
+                                
+                                <!-- other websites -->
+                                <!-- li><a href="https://twitter.com/h20ld3n" target="_blank"><span class="social social-rss"></span>http://flexboxfroggy.com/
+                                a game where you help Froggy and friends by writing CSS code!
+								I am currently on level 4</a></li-->                             
+                            </ul>
+                        </p>
+    				</div>
+    				<div class="col-md-3">
+                    	<p>
+      						<h3>Awards</h3>
+      						<ul>
+                            	<li class="h4">American Graphic Design Awards&nbsp;2015</li>
+      							<ul>
+                            		<li>Advanced Energy Now 2015 Market Report</li>
+                                	<li>California Advanced Energy Employment Future</li>
+                                	<li>Ohio's Electricity Future</li>
+                                	<li>North Carolina: Clean Energy Works!</li>
+                                	<li>Advanced Energy Leads California Jobs Growth</li>
+                            	</ul>
+	      						<li class="h4">PubWest Book Design Awards 2012</li>
+     	 						<ul>
+                            		<li>Enhanced e-Book, Silver Award</li>
+      								<li>Worse Case Scenario Jr. Interactive Adventure: Everest: You Decide How to Survive</li>
+                            	</ul>
+	      						<li class="h4">41st Annual Bookbuilders West Book Show 2012 - Certificate of Excellence</li>
+    							<ul>
+                            		<li>Worst-Case Scenario Ultimate Adventure: Everest</li>
+                            	</ul>
+	      						<li class="h4">Publishing Innovation Awards 2012 - Quality, Excellence, Design (QED)</li>
+								<ul>
+                            		<li>Count, Dagmar!</li>
+      								<li>Top Pot Hand-Forged Doughnuts</li>
+                            	</ul>
+      							<li class="h4">The 48 Hour Film Project 2009</li>
+                            	<ul>
+                            		<li>Best Acting Ensemble</li>
+								</ul>
+      							<li class="h4">Griffith Film School 2006</li>
+      							<ul>
+                            		<li>Award for Individual Excellence</li>
+                                	<li>Highly Commended Editing</li>
+                           		</ul>
+                            </ul>
+                     	</p>
+    				</div>
+    				<div class="col-md-6">
+      					<p>
+                        	<h3>Experience</h3>
+      						<dl>
+                            	<dt class="h4">Bleu Marketing Solutions</dt>
+                                <dd>(2016)<br>
+                                Oversaw the work of designers, illustrators and artists to produce campaign assets. Determined the overall style and tone for projects and articulated vision to other artists to deliver client presentations and final products. 
+                                </dd>
+                            	<dt class="h4">Advanced Energy Economy</dt>
+      							<dd>(2013 - 2016)<br>
+                               	Responsible for developing guidelines and directing creative work, including print collateral, websites, email campaigns, and related tasks. Work with and play an essential role in the marketing, communications, and digital departments to maximize the talents of copywriters, web developers, initiative leads, and other creatives.</dd>
+      							<dt class="h4">Gap Inc. (freelance)</dt>
+      							<dd>(2013)<br>
+                                Lead by positive and involved creative leadership, collaborated with copywriters, designers and marketing teams to create and support on-line and email campaigns.</dd>
+      							<dt class="h4">Chronicle Books</dt>
+      							<dd>(2010 - 2013)<br>
+      							Collaborated with authors, editors, designers, and outside vendors to create award-winning ebooks and mobile applications for various tablets and ebook readers. Formulated best practices and strategies for the creation and distribution of ebooks to various on-line retailers.</dd>
+      							<dt class="h4">TRX - Fitness Anywhere</dt>
+      							<dd>(2007 – 2010)<br>
+      							Worked with various teams to develop best practices for content creation and distribution, user interaction and experience for online communities, various landing pages, and large scale campaigns with the assistance of other designers and marketing managers.</dd>
+      							<dt class="h4">REO Company</dt>
+      							<dd>(2002 – 2005)<br>
+      							Worked with photographers, printers, and other vendors to design a consistent online and marketing presence.</dd>
+                                <dt class="h4">Four Eleven Productions (freelance)</dt>
+      							<dd>(2001 – 2005)<br>
+      							Created logos, websites, video, and various digital assets to support local and national campaigns for San Francisco based artists, non-profits, and start-ups.</dd>
+      							<dt class="h4">CBS MarketWatch</dt>
+                         		<dd>(1999 - 2001)<br>
+                                Collaborated with news and marketing teams to create online and printed materials.</dd>
+                           	</dl>
+      					</p>
+    				</div>
+  				</div>
+			</div>
+			<!--div style="text-align:center"> &copy;
+        	<?php 
+  			$fromYear = 1977; 
+  			$thisYear = (int)date('Y'); 
+  			echo $fromYear . (($fromYear != $thisYear) ? '-' . $thisYear : '');?>
+        	Holden Hardcastle </div>
+    		</div-->
+	</footer>
