675d67cedfd9c477573d1c166392a5d87dc0a27b
Holden Hardcastle <h0ld3n@four11productions.com>
2017-12-05 20:53:58 -0800

    Clean Up

--- js/chart.js
+++ js/chart.js
@@ -8,25 +8,25 @@ var randomScalingFactor = function() {
     var barChartData = {
         labels: ["wk 1", "wek 2", "wk 3", "wk 4", "wk 5"],
         datasets: [{
-            label: 'Prev. Month',
+            label: 'Applications',
             backgroundColor: "rgba(90,90,90,0.5)",
             data: [53, 56, 64, 62, 85],
             borderColor: 'white',
             borderWidth: 2
         }, {
-            label: 'Campaign Month',
+            label: '2X Applications',
             backgroundColor: "rgba(130,130,130,0.5)",
             data: [96, 193, 253, 229, 378],
             borderColor: 'white',
             borderWidth: 2
         }, {
             type: 'line',
-            label: 'Dataset 3',
+            label: 'Approvals',
             backgroundColor: "rgba(130,130,130,0.5)",
             data: [70, 120, 170, 230, 322],
 		}, {
            	type: 'line',
-            label: 'Dataset 4',
+            label: '2X Approvals',
             backgroundColor: "rgba(200,200,200,0.5)",
             data: [137, 342, 595, 824, 1202],
         }, ]
