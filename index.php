4e7f1aa32d208a447eaed6376dfcff430e00410c
Holden Hardcastle <h0ld3n@four11productions.com>
2018-11-27 21:31:28 -0800

    Portfolio Update
    Cleaned up header and improved navigation

--- index.php
+++ index.php
@@ -8,42 +8,21 @@
 	<meta name="keywords" content="HTML,CSS,XML,JavaScript">
 	<meta name="author" content="Holden Hardcastle">
 	<meta http-equiv="Cache-Control" content="max-age=200" />
-    <meta name="pinterest" content="nopin" />
-    <meta name="theme-color" content="#ffffff">
+	<meta name="pinterest" content="nopin" />
+	<meta name="theme-color" content="#ffffff">
 	<title>Holden Hardcastle</title>
-    <link href="https://fonts.googleapis.com/css?family=Abhaya+Libre" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="css/glyphicons-social.css">
-	<link rel="stylesheet" href="css/glyphicons-filetypes.css">
-    <link rel="stylesheet" type="text/css" href="css/golden-ratio-01.css">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+	<link href="https://fonts.googleapis.com/css?family=Abhaya+Libre" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="../css/glyphicons-social.css">
+	<link rel="stylesheet" href="../css/glyphicons-filetypes.css">
+	<link rel="stylesheet" type="text/css" href="css/golden-ratio-01.css">
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
 	<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
 	<link rel="manifest" href="/manifest.json">
 	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-	<script>!function(a,b,c,d,e){function g(a,c,d,e){var f=b.getElementsByTagName("script")[0];a.src=e,a.id=c,a.setAttribute("class",d),f.parentNode.insertBefore(a,f)}a.Mobify={points:[+new Date]};var f=/((; )|#|&|^)mobify=(\d)/.exec(location.hash+"; "+b.cookie);if(f&&f[3]){if(!+f[3])return}else if(!c())return;b.write('<plaintext style="display:none">'),setTimeout(function(){var c=a.Mobify=a.Mobify||{};c.capturing=!0;var f=b.createElement("script"),h="mobify",i=function(){var c=new Date;c.setTime(c.getTime()+3e5),b.cookie="mobify=0; expires="+c.toGMTString()+"; path=/",a.location=a.location.href};f.onload=function(){if(e)if("string"==typeof e){var c=b.createElement("script");c.onerror=i,g(c,"main-executable",h,mainUrl)}else a.Mobify.mainExecutable=e.toString(),e()},f.onerror=i,g(f,"mobify-js",h,d)})}(window,document,function(){a=/webkit|(firefox)[\/\s](\d+)|(opera)[\s\S]*version[\/\s](\d+)|(trident)[\/\s](\d+)/i.exec(navigator.userAgent);return!a||a[1]&&4>+a[2]||a[3]&&11>+a[4]||a[5]&&6>+a[6]?!1:!0},
-
-	// path to mobify.js
-	"//cdn.mobify.com/mobifyjs/build/mobify-2.0.16.min.js",
-
-	// calls to APIs go here (or path to a main.js)
-	function() {
-	 		var capturing = window.Mobify && window.Mobify.capturing || false;
-
-	  		if (capturing) {
-    		Mobify.Capture.init(function(capture){
-      		var capturedDoc = capture.capturedDoc;
-
-      		var images = capturedDoc.querySelectorAll("img, picture");
-      		Mobify.ResizeImages.resize(images);
-
-      		// Render source DOM to document
-      		capture.renderCapturedDoc();
-    		});
-  		}
-	});</script>
 </head>
 <body>
 	<h1 style="display:none;">Holden Hardcastle</h1>
@@ -63,18 +42,18 @@
 	</header>
 	<main>
    <section>
-  		<h2>Union Bank </h2>
-  		<article id="dell-emc-hybrid-cloud">
+	   <h2>Union Bank</h2>
+  		<article id="union-bank">
     		<div class="jumbotron" id="ub-cool-grey11">
       			<div class="container-fluid">
         			<div class="row">
-                    	<img src="images/wlt-ub-small-business-promo.png" alt="Union Bank" class="img-responsive">
+                    	<a name="wlt-ub"><img src="images/wlt-ub-small-business-promo.png" alt="Union Bank" class="img-responsive"></a>
                    	</div>
       			</div>
     		</div>
     		<div class="container">
       			<div class="row">
-					<img src="images/transparency.png"  alt="Union Bank" class="img-responsive">
+					<img src="images/transparency.png" class="img-responsive">
         			<div class="col-md-6 col-md-offset-3" id="description">
           				<h3>Union Bank Campaigns</h3>
 						<p>MUFG Union Bank aims to be a foundation of strength committed to meeting the needs of their customers, serving society, and fostering shared and sustainable growth for a better world.</p>
@@ -85,7 +64,7 @@
   						</dd>
 						</dl>
 						<p>By identifying the audience and messaging, cohesive campaigns were developed that adhered to brand guidelines and often resulted in a 400% increase in response rates.</p>
-						<p><button type="button" class="btn btn-light">Light</button></p>
+						<p><a class="btn btn-default" href="archive/wlt-union-bank.php" role="button" style="margin-top: 1em; width: 100%; font-size: .75em;">Back to main page</a></p>
                    	</div>
       			</div>
     		</div>
