b5151cded7eca76611732dde8cc75f3c109bc98d
Holden Hardcastle <h0ld3n@four11productions.com>
2019-01-04 22:53:25 -0800

    Portfolio Update - 0104

---
+++ archive/bleu-marketing.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<?php include '../includes/meta.php';?>
+	<title>Art Director — Bleu Marketing Solutions</title>
+	<?php include '../includes/link.php';?>
+</head>
+<body>
+	<h1 style="display:none;">Art Director — Bleu Marketing Solutions</h1>
+	<header>
+		<div class="jumbotron" id="holden-hardcastle">
+			<div class="container">
+    			<div class="row">
+					<div class="col-md-6 col-md-offset-3">
+        				<h1>Bleu Marketing Solutions</h1>
+        				<h2>Art Director</h2>
+                   		<p>(2016) Collaborated with clients and project leads to determine opportunities, goals, style, and tone for media campaigns. Communicated a vision and strategy to designers, illustrators, copywriters, and other artists. Approved artwork and delivered client presentations to review and finalize products.</p>
+  					</div>
+                </div>
+ 			</div>
+		</div>
+	</header>
+	<main>
+	<section>
+  		<h2>Dell EMC Hybrid Cloud and VCE Customer Value Infographics</h2>
+  		<article>
+    		<div class="jumbotron" id="bleu-blue">
+      			<div class="container-fluid">
+        			<div class="row">
+                    	<img src="../images/bleu-dell-emc-hybrid-cloud.png" alt="Dell EMC and the Hybrid Cloud" class="img-responsive">
+                    </div>
+      			</div>
+    		</div>
+    		<div class="container">
+      			<div class="row">
+        			<div class="col-md-6 col-md-offset-3" id="description">
+          				<h3>Dell EMC and the Hybrid Cloud</h3>
+          				<p>Dell EMC provides the foundation to help companies modernize, automate, and transform data centers by innovating across devices, ecosystems and services to design solutions for the way people work.</p>
+						<p>Identified audience, messaging, stories, and stats, to develop a cohesive campaign that adhered to brand guidelines. Digital content and collateral was designed to highlight metrics that influence CFO decision making.</p>
+						<p><a class="btn btn-default" href="../index.php#wlt-ub" role="button" style="margin-top: 1em; width: 100%; font-size: .75em;">Back to main page</a></p>
+                    </div>
+      			</div>
+    		</div>
+  		</article>
+		<article>
+    		<div class="jumbotron" id="bleu-blue">
+      			<div class="container-fluid">
+        			<div class="row">
+                    	<img src="../images/bleu-vce-customer-value.png" alt="VCE Customer Value" class="img-responsive">
+                    </div>
+      			</div>
+    		</div>
+    		<div class="container">
+      			<div class="row">
+        			<div class="col-md-6 col-md-offset-3" id="description">
+          				<h3>VCE Customer Value Infographic</h3>
+          				<p></p>
+						<p><a class="btn btn-default" href="../index.php#Union-Bank" role="button" style="margin-top: 1em; width: 100%; font-size: .75em;">Back to main page</a></p>
+                    </div>
+      			</div>
+    		</div>
+  		</article>
+	</section>
+    <?php include '../includes/footer.php';?>
+</main>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
+<script src="bootstrap/js/jquery-1.11.3.min.js"></script> 
+<script src="https://code.jquery.com/jquery-2.2.4.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.2.2/Chart.bundle.js"></script>
+<script  src="js/chart.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+</body>
+</html>
--- index.php
+++ index.php
@@ -75,9 +75,10 @@
     		<div class="container">
       			<div class="row">
         			<div class="col-md-6 col-md-offset-3" id="description">
-          				<h3>Dell EMC and the Hybrid Cloud</h3>
-						<p>Dell EMC provides the foundation to help companies  modernize, automate, and transform data centers by innovating across devices, ecosystems and services to design solutions for the way people work.</p>
-                   	    <p>Identified audience, messaging, stories, and stats, to develop a cohesive campaign that adhered to brand guidelines. Digital content and collateral was designed to highlight metrics that influence CFO decision making.</p>
+          				<h3>Bleu Marketing Solutions Inc.—Art Director</h3>
+						<p>(2016) Collaborated with clients and project leads to determine opportunities, goals, style, and tone for media campaigns. Communicated a vision and strategy to designers, illustrators, copywriters, and other artists. Approved artwork and delivered client presentations to review and finalize products.</p>
+						<p>Dell EMC and the Hybrid Cloud — Dell EMC provides the foundation to help companies  modernize, automate, and transform data centers by innovating across devices, ecosystems and services to design solutions for the way people work.</p>
+                   	    <p>Identified audience, messaging, stories, and stats, to develop a cohesive launch campaign that adhered to brand guidelines. Digital content and collateral was designed to highlight metrics that influence CFO decision making.</p>
                    	</div>
       			</div>
     		</div>
@@ -85,98 +86,20 @@
 	</section>
 	<section>
 		<h2>Advanced Energy Economy</h2>
-		<article>
-    		<div class="jumbotron" id="aee-blue">
-    			<div class="container-fluid">
-        			<div class="row">
-                    	<img src="images/aee-letterhead.png" alt="Advanced Energy Economy Brand and Style Guide" class="img-responsive">
-                  	</div>
-      			</div>
-    		</div>
-    		<div class="container">
-    			<div class="row">
-        			<div class="col-md-6 col-md-offset-3" id="description">
-          				<h3>AEE Letterhead<br> and Branding</h3>
-          				<p>Advanced Energy Economy engages with companies in order to create a more prosperous world that runs on secure, clean, and affordable energy.</p>
-          				<p>The design values complement the company vision. The brand guidelines aim to create a consistent, clean, and efficient experience. This solid foundation allows for evolution. Corporate logos, iconography, infographics, reports, responsive websites, and members of the AEE team, have all played a part in the progression of the Advanced Energy Economy brand.</p>
-        			</div>
-      			</div>
-    		</div>
-  		</article>
   		<article id="aee-website-process">
     		<div class="jumbotron" id="aee-blue">
       			<div class="container-fluid">
         			<div class="row">
-                    	<img src="images/aee-initiatives.png"  alt="Advanced Energy Economy Responsive Website" class="img-responsive">
+                    	<a name="wlt-ub"><img src="images/aee-initiatives.png"  alt="Advanced Energy Economy Responsive Website" class="img-responsive"></a>
                    	</div>
       			</div>
     		</div>
     		<div class="container">
       			<div class="row">
         			<div class="col-md-6 col-md-offset-3" id="description">
-          				<h3>AEE Website and<br> Process Redesign</h3>
-          				<p>In order to create a prosperous future, Advanced Energy Economy (AEE) envisions a world that runs on secure, clean, and affordable energy. AEE engages member companies, and recruits partners through education, and encourages participation in energy policy changing initiatives.</p>
-         				<p>Policy and legislation changes quickly. In order to keep up emerging energy regulations, the AEE website needed to be redesigned and migrated to a content management system (CMS). By implementing a CMS, projects leads would be able to make timely content updates.</p>
-          				<p>Working with the communications, marketing and development teams we identified and implemented specific improvements on both the front and backend.</p>
-          				<p>By building templates on a flexible and responsive Bootstrap foundation, content updates became more efficient and streamlined the look and feel of the site.</p>
-        			</div>
-      			</div>
-    		</div>
-  		</article>
-  		<article>
-    		<div class="jumbotron" id="aee-blue">
-      			<div class="container-fluid">
-        			<div class="row">
-                    	<img src="images/aee-covers.png"  alt="Advanced Energy Economy & AEE Institute Reports" class="img-responsive">
-                   	</div>
-      			</div>
-    		</div>
-    		<div class="container">
-      			<div class="row">
-        			<div class="col-md-6 col-md-offset-3" id="description">
-          				<h3>Advanced Energy<br> Economy Reports</h3>
-          				<p>The educational reports focus on the technical, economic, social, and political issues surrounding advanced energy, and how to improve the energy sector. To engage member companies the reports are available for download, distributed at events, and presented at congressional hearings.</p>
-          				<p>By collaborating with the communications team and project leads we implemented the newly established brand guidelines to define and apply consistent hierarchy and visual style to reports. Covers communicate the subject of the complex reports in a single image.</p>
-          				<p>Using a grid system and dynamically linking text to design files, the annual and semi-annual reports are effortlessly designed and produced. The powerful imagery and iconography create a visual message that accelerates the transition to an advanced energy economy.</p>
-          				<p>The California Advanced Energy Employment Survey report won a 2015 American Graphic Design Award.</p>
-        			</div>
-      			</div>
-    		</div>
-  		</article>
-  		<article>
-    		<div class="jumbotron" id="aee-blue">
-      			<div class="container-fluid">
-        			<div class="row">
-                    	<img src="images/aee-ohio-electricity-future.png" alt="Ohio's Electricity Future Report" class="img-responsive">
-                   	</div>
-      			</div>
-    		</div>
-    		<div class="container">
-      			<div class="row">
-        			<div class="col-md-6 col-md-offset-3" id="description">
-          				<h3>Ohio's Electricity<br> Future Report</h3>
-          				<p>The Ohio Advanced Energy Economy, an Advanced Energy Economy State Partner, was looking to create brand guidelines and an annual report that appealed to their members. They reached out to AEE to create impactful design standards that represented their messaging.</p>
-          				<p> A logo, font face, and collateral were structured around the&nbsp;established AEE design guidelines. The newly created brand visually identified Ohio as part of the extended AEE family.</p>
-          				<p>The report on the state of Ohio's Electricity Future won a 2015 American Graphic Design Award.</p>
-        			</div>
-      			</div>
-    		</div>
-  		</article>
-  		<article>
-    		<div class="jumbotron" id="aee-blue">
-      			<div class="container-fluid">
-        			<div class="row">
-                    	<img src="images/aee-infographics.png" alt="AEE and AEE Institute Infographics" class="img-responsive">
-                   	</div>
-      			</div>
-    		</div>
-    		<div class="container">
-      			<div class="row">
-        			<div class="col-md-6 col-md-offset-3" id="description">
-          				<h3>AEE and AEE<br> Institute Infographics</h3>
-          				<p>Advanced Energy Economy initiative leads present data and reports to companies and legislatures across all levels of government. Infographics are created to present complex information and tell the story behind the energy data in an easily understandable way.</p>
-          				<p>Working with communications and initiative leads, we distilled complex data down to identify key messaging. The resulting visuals are designed to support arguments that move executives and government officials to action.</p>
-          				<p>The North Carolina: Clean Energy Works! and California Advanced Energy Employment Future infographics both won 2015 American Graphic Design Awards.</p>
+          				<h3>Advanced Energy Economy—Creative Director</h3>
+          				<p>(2013-2016) Developed design guidelines and directed creative work, including printed collateral, websites, and email campaigns. Played an essential role in the marketing, communications, and digital departments to maximize the talents of copywriters, web developers, initiative leads, and other creatives.</p>
+						<p><a class="btn btn-default" href="archive/advanced-energy-economy.php" role="button" style="margin-top: 1em; width: 100%; font-size: .75em;">Discover more of the AEE branding</a></p>
         			</div>
       			</div>
     		</div>
