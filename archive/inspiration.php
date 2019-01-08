1faeda755a26ef921f7f70e481eb49b1ffbc3150
Holden Hardcastle <h0ld3n@four11productions.com>
2019-01-07 20:41:24 -0800

    Inspiration

---
+++ archive/inspiration.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<?php include '../includes/meta.php';?>
+	<title>Holden Hardcastle's Inspiration</title>
+	<?php include '../includes/link.php';?>
+</head>
+<body>
+	<h1 style="display:none;">Holden Hardcastle</h1>
+	<?php include '../includes/navbar.php';?>
+	<header>
+		<div class="jumbotron" id="holden-hardcastle">
+			<div class="container">
+    			<div class="row">
+					<div class="col-md-6 col-md-offset-3">
+        				<h1>Inspiration</h1>
+        				<h2>The action or power of moving the intellect or emotions.</h2>
+                   		<p>The act of influencing or suggesting opinions, drawing in specifically, quality or state of being inspired, something that is motivated, an inspiring agent or influence.</p>
+  					</div>
+                </div>
+ 			</div>
+		</div>
+	</header>
+	<main>
+	<section>
+		<h2>Tonight's the Night</h2>
+		<article>
+    		<div class="jumbotron" id="inspiration">
+      			<div class="container-fluid">
+        			<div class="row">
+                    	<img src="../images/inspiration/190107-inspiration.png" alt="Hoo Ru Da San" class="img-responsive">
+                    </div>
+      			</div>
+    		</div>
+    		<div class="container">
+      			<div class="row">
+        			<div class="col-md-6 col-md-offset-3" id="description">
+          				<h3>Three dimensions</h3>
+						<p>Color has three dimensions which can be defined and measured. Hue is the color itself, or chroma, of which there are more than a hundred. Each hue has individual characteristics; group or categories of colors share common effects. There are three primary or elementary hues: yellow, red, blue. Each represents qualities that are fundamental. Yellow is the color considered closest to light and warmth; red is the most emotional and active; blue is passive and soft. Yellow and red tend to expand, blue contract. In association with each, through moxtures, new meanings take over. Red, which is a provoking hue, is subdued when it is mixed with blue and activated when it is combined with yellow. The same changes in effect are reached with yellow, which mellows when it is mixed with blue. </p>
+						<p><a class="btn btn-default" href="../archive/sketchbook.php" role="button" style="margin-top: 1em; width: 100%; font-size: .75em;">Previous sketchbook</a></p>
+                    </div>
+      			</div>
+    		</div>
+  		</article>
+		</section>
+    <?php include '../includes/footer.php';?>
+</main>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="../bootstrap/js/jquery-1.11.3.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<!-- script src="js/bootstrap.min.js"></script-->
+</body>
+</html>
