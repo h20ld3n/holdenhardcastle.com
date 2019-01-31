6adf577e270606ff4c7e7aba96b3d5f2a59206a7
Holden Hardcastle <h0ld3n@four11productions.com>
2019-01-30 19:57:41 -0800

    Adding Dimension

--- archive/inspiration.php
+++ archive/inspiration.php
@@ -28,6 +28,23 @@
     		<div class="jumbotron" id="inspiration">
       			<div class="container-fluid">
         			<div class="row">
+                    	<img src="../images/inspiration/190130-inspiration.png" alt="Me Looking at a bridge" class="img-responsive">
+                    </div>
+      			</div>
+    		</div>
+    		<div class="container">
+      			<div class="row">
+        			<div class="col-md-6 col-md-offset-3" id="description">
+          				<h3>Dimension</h3>
+						<p>Representation of dimension in two-dimensional visual formats is also dependent on illusion. Dimension exist in the real world. We cannot only feel it but with the aid of our two-eyed, stereopticon sight, we can see it. But in all two-dimensional representations of reality in drawing, painting, photography, film, television, there is no actualized dimension; it is only implied. The illusion is reinforced in many ways, but the prime device for simulating dimension is the technical convention of perspective. The effects produced by perspective can be enhanced by tonal manipulation through "chiaroscuro," the dramatic emphasis of light and shade. </p>
+                    </div>
+      			</div>
+    		</div>
+  		</article>
+		<article>
+    		<div class="jumbotron" id="inspiration">
+      			<div class="container-fluid">
+        			<div class="row">
                     	<img src="../images/inspiration/190129-inspiration.png" alt="Me Looking at a bridge" class="img-responsive">
                     </div>
       			</div>
