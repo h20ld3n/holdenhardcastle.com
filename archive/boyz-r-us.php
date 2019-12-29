b5069eb783fe2680533f88bcf6ac1162e42004aa
Holden Hardcastle <h0ld3n@four11productions.com>
2019-12-28 21:25:34 -0800

    bru-beginning

---
+++ archive/boyz-r-us.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<?php include '../includes/meta.php';?>
+	<title>BOYZ 'R' US</title>
+	<?php include '../includes/link.php';?>
+</head>
+<body>
+	<h1 style="display:none;">Holden Hardcastle</h1>
+	<?php include '../includes/navbar.php';?>
+	<header>
+		<div class="jumbotron" id="holden-hardcastle">
+			<div class="container">
+    			<div class="row">
+					<div class="col-md-6 col-md-offset-3">
+        				<h1>BOYZ 'R' US</h1>
+        				<h2>Adapted from the novel by Scott Monk</h2>
+                   		<p>Screenplay by Holden Hardcastle. 3rd Draft.</p>
+  					</div>
+                </div>
+ 			</div>
+		</div>
+	</header>
+	<main>
+		<section>
+		<article>
+    		<div class="container">
+      			<div class="row">
+        			<div class="col-md-10 col-md-offset-1" id="description">
+						<ul class="screenbox">
+							<li class="sceneheader">INT. DAY - HIGH SCHOOL CLASSROOM</li>
+							<li class="action">A weathered and scared man in a grey prison uniform is...</li>
+							<li class="character">WHEELER</li>
+							<li class="dialogue">People said I'd end up in jail. Guess it just took longer than expected. I can't blame 'em. I was always into trouble. Pigs hassling me, judges threatening to lock me up. I should have listened. I wouldn't be eating lumpy food and showering with every murder who's been on the news. You've got no idea. it's not like the tele. It's a tomb. You're not living at all. Everything's grey, floors, walls, meals...hope. You go crazy , the same god damn color staring at you everyday. Windows keep you sane. Waking up to see the blue skies and the white clouds, it's a depressing happiness. But no one cares. That's not what people are interested in. That's not what you're interested in. You want to know why, why, why I'm here? Well, everyone's got a story. I suppose mine's as good as any...to protect you from people like me. Once your inside, in it, in it for real...it's over. </li>
+						</ul>
+                    </div>
+      			</div>
+    		</div>
+  		</article>
+		<article>
+    		<div class="container">
+      			<div class="row">
+        			<div class="col-md-10 col-md-offset-1" id="description">
+						<ul class="screenbox">
+							<li class="sceneheader">INT. DAY - HIGH SCHOOL BATHROOM</li>
+							<li class="action">Four kids, Flash Jack, Peeper, and Marc, are dressed in American street wear are standing around listening to Barry Wheeler rant.</li>
+							<li class="action">Barry Wheeler is a tall 16-year kid.</li>
+							<li class="action">Peeper is wearing a Malcolm X baseball hat.</li>
+							<li class="action">Marc is tall and built like a rugby player.</li>
+							<li class="character">WHEELER</li>
+							<li class="dialogue">Where the fuck's he been lately?</li>
+							<li class="character">PEEPER</li>
+							<li class="dialogue">He just got stabbed.</li>
+							<li class="character">FLASH JACK</li>
+							<li class="dialogue">You ever been stabbed?</li>
+							<li class="character">WHEELER</li>
+							<li class="dialogue">Fuck all that. Little pussy-boy can't handle anything.</li>
+							<li class="action">One of the bathroom stalls slams open and a small and skinny year-nine runs out of the bathroom.</li>
+						</ul>
+                    </div>
+      			</div>
+    		</div>
+  		</article>
+			<article>
+    		<div class="container">
+      			<div class="row">
+        			<div class="col-md-10 col-md-offset-1" id="description">
+						<ul class="screenbox">
+							<li class="sceneheader">INT. DAY - HIGH SCHOOL NURSE'S OFFICE</li>
+							<li class="action">A cot is set up next to the wall below a window.</li>
+							<li class="action">A nurse opens the door. Mitch is standing next to her trying his best to look sick.</li>
+							<li class="action">Mitch is an 18-year old, wearing black American street wear.</li>
+							<li class="character">NURSE</li>
+							<li class="dialogue">You can lie there till you're right.</li>
+							<li class="action">Mitch walks through the door and sits on the cot.</li>
+							<li class="character">MITCH</li>
+							<li class="dialogue">Thanks.</li>
+						</ul>
+                    </div>
+      			</div>
+    		</div>
+  		</article>
+		</section>
+    <?php include '../includes/footer-social.php';?>
+</main>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="../bootstrap/js/jquery-1.11.3.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<!-- script src="js/bootstrap.min.js"></script-->
+</body>
+</html>
--- includes/link.php
+++ includes/link.php
@@ -4,7 +4,7 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="../css/glyphicons-social.css">
 <link rel="stylesheet" href="../css/glyphicons-filetypes.css">
-<link rel="stylesheet" type="text/css" href="../css/golden-ratio-01.css">
+<link rel="stylesheet" type="text/css" href="../css/golden-ratio-1.618.css">
 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
 <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
