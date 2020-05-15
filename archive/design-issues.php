5bb86a2a36c0af68241ca7768134005231056f79
Holden Hardcastle <h0ld3n@four11productions.com>
2020-05-14 19:25:59 -0700

    design issues 0514

--- archive/design-issues.php
+++ archive/design-issues.php
@@ -28,15 +28,32 @@
     		<div class="jumbotron" id="design-issues">
       			<div class="container-fluid">
         			<div class="row">
-                    	<img src="../images/inspiration/190503-inspiration.png" alt="random pages from my sketchbook" class="img-responsive">
+                    	<img src="../images/design-issues/200514-design-issues.png" alt="random pages from my sketchbook" class="img-responsive">
                     </div>
       			</div>
     		</div>
     		<div class="container">
       			<div class="row">
         			<div class="col-md-6 col-md-offset-3" id="description">
-          				<h3>Color structure valuation is taught thought the color wheel.</h3>
+          				<h3>Color structure valuation is taught through the color wheel.</h3>
 						<p> The primary colors, yellow, red, blue, and secondary colors, orange, green, violet, are invariably included in such a chart. But it usually also includes expanded mixtures of at least twelve hues. From this simple chart of a color wheel, multiple variations of color hues can be developed, which may or may not suggest to each other under certain circumstances. The judgement of the eye is usually checked on by the hand by actual touching. Is it really smooth, or does it just look that way? Is that an indentation or a raised mark? No wonder there are so many "DO NOT TOUCH!" signs. </p>
+                    </div>
+      			</div>
+    		</div>
+  		</article>
+			<article>
+    		<div class="jumbotron" id="design-issues">
+      			<div class="container-fluid">
+        			<div class="row">
+                    	<img src="../images/design-issues/200515-design-issues.png" alt="random pages from my sketchbook" class="img-responsive">
+                    </div>
+      			</div>
+    		</div>
+    		<div class="container">
+      			<div class="row">
+        			<div class="col-md-6 col-md-offset-3" id="description">
+          				<h3>Texture has reference to the composition of a substance through minute variations on the surface material.</h3>
+						<p>Texture should serve as a sensitive and enriching experience. Unfortunately, those "DO NOT TOUCH! signs in the expensive shops overlap into social behavior, and we are strongly conditioned not to touch things or people with anything approximating sensual involvement. The result is a minimal tactile experience and even a fear of tactile contact; the sense of blind touch is carefully guarded in sighted people. We act super-cautiously when blindfolded or in the dark, reaching out tentatively, and, because of our limited experience of touch, we often do not recognize a texture. At the 1967 Montreal Expo, the 5+ Coming Pavilion was designed for visitors to explore the quality of their five senses. It was a popular and enjoyable exhibit. People sniffed away at a series of funnels offering a variety of odors, even though they suspected, and justifiably, that some would be unpleasant. They listened, they looked, they tasted, but they stood hesitant and inhibited in front of the yawning holes designed to be reached inro blindly. What did they fear? it appears that the natural, free, "hands-on" investigative approach of the baby and young child has been conditioned out of the adult by—Who knows what?—the Anglo-Saxon ethic, Puritan repression, instinctive taboos. Whatever the reason, the result starves one of our richest senses. But in this increasingly simulated and plastic world, the problem arises infrequently. Most of our textural experience is optical, not tactile. Not only is texture faked rather convincingly in plastics and printed material and faked fur, but, also, much of what we see that is painted, photographed, and filmed convincingly presents texture that is not there. If we ouch a photograph of silky velvet, we do not have the convincing tactile experience the visual clues promise. Meaning is based on what we see. This fakery is an important factor in survival in nature; animals, birds, reptiles, insects, fish, take on the coloration and texture of their surroundings as a protection against predators. Man copies this camouflage method in war in response to the same needs for survival that inspires it in nature.  </p>
                     </div>
       			</div>
     		</div>
