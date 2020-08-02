ad906bacf2389ebe38e6656e5986164edd145beb
Holden Hardcastle <h0ld3n@four11productions.com>
2020-08-02 15:43:16 -0700

    Social Footer Updates

--- includes/footer-social.php
+++ includes/footer-social.php
@@ -2,7 +2,7 @@
 		<div class="jumbotron">
 			<div class="container-fluid">
   				<div class="row text-muted">
-    				<div class="col-md-3">
+    				<div class="col-md-3 col-md-offset-1">
                     	<p>
                         	<h3>About</h3>
                  			Throughout my professional career I have collaborated with teams at Apple, Amazon, Sony, HBO, and Lucasfilm. I've worked with authors, editors, and artists to push creative boundaries. I thrive on generously sharing ideas, resources, and information with my peers. Whether it's bringing people together through an application or developing a dynamic brand to change the world, I believe that inspired teams yield inspirational results.      						
@@ -16,7 +16,50 @@
 							<dl class="h4"><a href="../archive/david-carson.php">A Conversation</a></dl>
     	  						<dt>with David Carson</dt>
                                 <dd>from EMIGRE</dd>
-                            <h3>Contact and Other Sites</h3>
+                            
+                        </p>
+    				</div>
+    				<div class="col-md-3 col-md-offset-1">
+                    	<p>
+      						<h3>Awards &amp; Recognition</h3>
+      						<ul>
+                            	<li class="h4">American Graphic Design Awards&nbsp;2015</li>
+      							<ul>
+                            		<li>Advanced Energy Now 2015 Market Report</li>
+                                	<li>California Advanced Energy Employment Future</li>
+                                	<li>Ohio's Electricity Future</li>
+                                	<li>North Carolina: Clean Energy Works!</li>
+                                	<li>Advanced Energy Leads California Jobs Growth</li>
+                            	</ul>
+	      						<li class="h4">PubWest Book Design Awards 2012</li>
+     	 						<ul>
+                            		<li>Enhanced e-Book, Silver Award</li>
+      								<li>Worse Case Scenario Jr. Interactive Adventure: Everest: You Decide How to Survive</li>
+                            	</ul>
+	      						<li class="h4">41st Annual Bookbuilders West Book Show 2012 - Certificate of Excellence</li>
+    							<ul>
+                            		<li>Worst-Case Scenario Ultimate Adventure: Everest</li>
+                            	</ul>
+	      						<li class="h4">Publishing Innovation Awards 2012 - Quality, Excellence, Design (QED)</li>
+								<ul>
+                            		<li>Count, Dagmar!</li>
+      								<li>Top Pot Hand-Forged Doughnuts</li>
+                            	</ul>
+      							<li class="h4">The 48 Hour Film Project 2009</li>
+                            	<ul>
+                            		<li>Best Acting Ensemble</li>
+								</ul>
+      							<li class="h4">Griffith Film School 2006</li>
+      							<ul>
+                            		<li>Award for Individual Excellence</li>
+                                	<li>Highly Commended Editing</li>
+                           		</ul>
+                            </ul>
+                     	</p>
+    				</div>
+                    <div class="col-md-2 col-md-offset-1">
+                    	<p>
+      						<h3>Contact and Other Sites</h3>
                             <ul>
                             	<li><a href="mailto:holden@holdenhardcastle.com"><span class="social social-e-mail"></span>holden@holdenhardcastle.com</a></li>
 								<li><a href="https://www.linkedin.com/in/holdenhardcastle" target="_blank"><span class="social social-linked-in"></span>linkedin</a></li>
@@ -53,79 +96,7 @@
                                 
                                                          
                             </ul>
-                        </p>
-    				</div>
-    				<div class="col-md-3">
-                    	<p>
-      						<h3>Awards</h3>
-      						<ul>
-                            	<li class="h4">American Graphic Design Awards&nbsp;2015</li>
-      							<ul>
-                            		<li>Advanced Energy Now 2015 Market Report</li>
-                                	<li>California Advanced Energy Employment Future</li>
-                                	<li>Ohio's Electricity Future</li>
-                                	<li>North Carolina: Clean Energy Works!</li>
-                                	<li>Advanced Energy Leads California Jobs Growth</li>
-                            	</ul>
-	      						<li class="h4">PubWest Book Design Awards 2012</li>
-     	 						<ul>
-                            		<li>Enhanced e-Book, Silver Award</li>
-      								<li>Worse Case Scenario Jr. Interactive Adventure: Everest: You Decide How to Survive</li>
-                            	</ul>
-	      						<li class="h4">41st Annual Bookbuilders West Book Show 2012 - Certificate of Excellence</li>
-    							<ul>
-                            		<li>Worst-Case Scenario Ultimate Adventure: Everest</li>
-                            	</ul>
-	      						<li class="h4">Publishing Innovation Awards 2012 - Quality, Excellence, Design (QED)</li>
-								<ul>
-                            		<li>Count, Dagmar!</li>
-      								<li>Top Pot Hand-Forged Doughnuts</li>
-                            	</ul>
-      							<li class="h4">The 48 Hour Film Project 2009</li>
-                            	<ul>
-                            		<li>Best Acting Ensemble</li>
-								</ul>
-      							<li class="h4">Griffith Film School 2006</li>
-      							<ul>
-                            		<li>Award for Individual Excellence</li>
-                                	<li>Highly Commended Editing</li>
-                           		</ul>
-                            </ul>
                      	</p>
-    				</div>
-    				<div class="col-md-6">
-      					<p>
-                        	<h3>Experience</h3>
-      						<dl>
-                            	<dt class="h4">Williams Lea Tag</dt>
-                                <dd>(2016-Present)<br>
-                                Create and present strategy concepts to clients and senior executives. Direct designers, production artist, copywriters, and other specialists to create effective advertising materials for multi-channel (print, packaging, digital, and TV) campaigns.</dd>
-                               	<dt class="h4">Bleu Marketing Solutions</dt>
-                                <dd>(2016)<br>
-                                Collaborated with clients and project leads to determine opportunities, goals, style, and tone for media campaigns. Communicated a vision and strategy to designers, illustrators, copywriters, and other artists. Approved artwork and delivered client presentations to review and finalize products.</dd>
-                            	<dt class="h4">Advanced Energy Economy</dt>
-      							<dd>(2013-2016)<br>
-                               	Developed design guidelines and directed creative work, including printed collateral, websites, and email campaigns. Played an essential role in the marketing, communications, and digital departments to maximize the talents of copywriters, web developers, initiative leads, and other creatives.</dd>
-      							<dt class="h4">Gap Inc. (freelance)</dt>
-      							<dd>(2013)<br>
-                                Directed by positive and involved creative leadership, collaborated with copywriters, designers, and marketing teams to create and support online and email campaigns.</dd>
-      							<dt class="h4">Chronicle Books</dt>
-      							<dd>(2010-2013)<br>
-      							Collaborated with authors, editors, designers, and outside vendors to create award-winning ebooks and applications, for various mobile devices, tablets, and ebook readers. Formulated best practices and strategies for the creation and distribution of ebooks to online retailers. </dd>
-      							<dt class="h4">TRX - Fitness Anywhere</dt>
-      							<dd>(2007–2010)<br>
-      							Worked with various teams to develop best practices for content creation and distribution, user interaction and experience for online communities, landing pages, and large scale campaigns with the assistance of other designers and marketing managers.</dd>
-      							<dt class="h4">REO Company</dt>
-      							<dd>(2002–2005)<br>
-      							Led photographers, printers, and other vendors to design a consistent online and marketing presence.</dd>
-                                <dt class="h4">Four Eleven Productions (freelance)</dt>
-      							<dd>(2001–2005)<br>
-      							Created logos, websites, videos, and various digital assets to support local and national campaigns for San Francisco based artists, non-profits, and startups.</dd>
-      							<dt class="h4"><a href="../archive/cbs-marketwatch.php">CBS MarketWatch</a></dt>
-                         		<dd>(1999-2001)<br>
-                                Collaborated with news, and marketing teams to create online and printed materials.</dd>
-                           	</dl>
-      					</p>
     				</div>
   				</div>
 			</div>
