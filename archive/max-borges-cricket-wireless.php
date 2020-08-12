a2049077891da1f2eda35e4a867e9af9bb29d8d0
Holden Hardcastle <h0ld3n@four11productions.com>
2020-08-12 14:05:11 -0700

    Cricket Wireless Case Study—Setup

---
+++ archive/max-borges-cricket-wireless.php
@@ -0,0 +1,133 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<?php include '../includes/meta.php';?>
+	<title>Max Borges — Cricket Wireless</title>
+	<?php include '../includes/link.php';?>
+</head>
+<body>
+	<h1 style="display:none;">Case Study — Cricket Wireless</h1>
+	<?php include '../includes/navbar.php';?>
+	<header>
+		<div class="jumbotron" id="holden-hardcastle">
+			<div class="container">
+    			<div class="row">
+					<div class="col-md-6 col-md-offset-3">
+        				<h1>Max Borges — Cricket Wireless Case Study</h1>
+        				<h2>We’re here with a reliable network</h2>
+                   		<p><strong>At Cricket Wireless everything they do makes it easier for everyone to connect</strong>. They cover 99% of Americans by providing affordable cell phones and plans that run on a high quality realiable network.</p>
+						<p>The relationship cultivating strategy kicks off by generating awareness and top-level education about products. The next interest phase cues people and potential customers into what the company stands for and who they are. Media personalities and influencers continue to cultivate and validate the brand in the next phase, eventually leading to a sale. Post sale—the relationship continues to grow as users become advocates.</p>
+  					</div>
+                </div>
+ 			</div>
+		</div>
+	</header>
+	<main>
+	<section>
+		<h2>Cricket Wireless Case Study</h2>
+		<article>
+    		<div class="jumbotron" id="aee-blue">
+    			<div class="container-fluid">
+        			<div class="row">
+                    	<img src="../images/max-borges-cricket-wireless-0939.png" alt="Advanced Energy Economy Brand and Style Guide" class="img-responsive">
+                  	</div>
+      			</div>
+    		</div>
+    		<div class="container">
+    			<div class="row">
+        			<div class="col-md-6 col-md-offset-3" id="description">
+          				<h3>Awareness</h3>
+          				<p>Tom Steyer co-founded Advanced Energy Economy to engage with companies in order to create a more prosperous world that runs on secure, clean, and affordable energy.</p>
+          				<p>The design values complement the company vision. The brand guidelines aim to create a consistent, clean, and efficient experience. This solid foundation allows for evolution. Corporate logos, iconography, infographics, reports, responsive websites, and members of the AEE team, have played a part in the progression of the brand.</p>
+        			</div>
+      			</div>
+    		</div>
+  		</article>
+  		<article id="aee-website-process">
+    		<div class="jumbotron" id="aee-blue">
+      			<div class="container-fluid">
+        			<div class="row">
+                    	<img src="../images/aee-initiatives.png"  alt="Advanced Energy Economy Responsive Website" class="img-responsive">
+                   	</div>
+      			</div>
+    		</div>
+    		<div class="container">
+      			<div class="row">
+        			<div class="col-md-6 col-md-offset-3" id="description">
+          				<h3>AEE Website and<br> Process Redesign</h3>
+          				<p>AEE engages member companies and recruits partners through education and encourages participation in energy policy changing initiatives. Policy and legislation changes quickly. In order to keep up emerging energy regulations, the AEE website needed to be redesigned and migrated to a content management system (CMS). By implementing a CMS, projects leads would be able to make timely content updates.</p>
+          				<p>Working with the communications, marketing and development teams, we identified and implemented specific improvements on both the front and backend. By building templates on a flexible and responsive Bootstrap foundation, content updates became more efficient and streamlined the look and feel of the site.</p>
+        			</div>
+      			</div>
+    		</div>
+  		</article>
+  		<article>
+    		<div class="jumbotron" id="aee-blue">
+      			<div class="container-fluid">
+        			<div class="row">
+                    	<img src="../images/aee-covers.png"  alt="Advanced Energy Economy & AEE Institute Reports" class="img-responsive">
+                   	</div>
+      			</div>
+    		</div>
+    		<div class="container">
+      			<div class="row">
+        			<div class="col-md-6 col-md-offset-3" id="description">
+          				<h3>Advanced Energy<br> Economy Reports</h3>
+          				<p>The educational reports focus on the technical, economic, social, and political issues surrounding advanced energy and how to improve the energy sector. To engage member companies the reports are available for download, distributed at events, and presented at congressional hearings.</p>
+						<p>We implemented newly established brand guidelines to define and apply consistent visual hierarchies and styles to reports. Covers communicate the subject of the complex reports in a single image. By using a grid system and dynamically linking text to design files, the annual and semi-annual reports were seamlessly designed and produced. The powerful imagery and iconography create a visual message that accelerates the transition to an advanced energy economy.</p>
+          				<p>The California Advanced Energy Employment Survey report won a 2015 American Graphic Design Award.</p>
+        			</div>
+      			</div>
+    		</div>
+  		</article>
+  		<article>
+    		<div class="jumbotron" id="aee-blue">
+      			<div class="container-fluid">
+        			<div class="row">
+                    	<img src="../images/aee-ohio-electricity-future.png" alt="Ohio's Electricity Future Report" class="img-responsive">
+                   	</div>
+      			</div>
+    		</div>
+    		<div class="container">
+      			<div class="row">
+        			<div class="col-md-6 col-md-offset-3" id="description">
+          				<h3>Ohio's Electricity<br> Future Report</h3>
+          				<p>The Ohio Advanced Energy Economy, an Advanced Energy Economy State Partner, was looking to create brand guidelines and an annual report that appealed to their members. They reached out to AEE to create impactful design standards that represented their messaging.</p>
+          				<p>A logo, font face, and collateral were structured around the&nbsp;established AEE design guidelines. The newly created brand visually identified Ohio as part of the extended AEE family.</p>
+          				<p>The report on the state of Ohio's Electricity Future won a 2015 American Graphic Design Award.</p>
+        			</div>
+      			</div>
+    		</div>
+  		</article>
+  		<article>
+    		<div class="jumbotron" id="aee-blue">
+      			<div class="container-fluid">
+        			<div class="row">
+                    	<img src="../images/aee-infographics.png" alt="AEE and AEE Institute Infographics" class="img-responsive">
+                   	</div>
+      			</div>
+    		</div>
+    		<div class="container">
+      			<div class="row">
+        			<div class="col-md-6 col-md-offset-3" id="description">
+          				<h3>AEE and AEE<br> Institute Infographics</h3>
+          				<p>Advanced Energy Economy initiative leads present data and reports to companies and legislatures across all levels of government. Infographics were created to present complex information and tell the story behind the energy data in an easily understandable way.</p>
+						<p>We distilled complex data down to identify key messaging. The resulting visuals were designed to support arguments that move executives and government officials to action.</p>
+          				<p>The North Carolina: Clean Energy Works! and California Advanced Energy Employment Future infographics both won 2015 American Graphic Design Awards.</p>
+						<p><a class="btn btn-default" href="../index.php#aee" role="button" style="margin-top: 1em; width: 100%; font-size: .75em;">Back to main page</a></p>
+        			</div>
+      			</div>
+    		</div>
+  		</article>
+	</section>
+    <?php include '../includes/footer.php';?>
+</main>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
+<script src="bootstrap/js/jquery-1.11.3.min.js"></script> 
+<script src="https://code.jquery.com/jquery-2.2.4.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.2.2/Chart.bundle.js"></script>
+<script  src="js/chart.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+</body>
+</html>
