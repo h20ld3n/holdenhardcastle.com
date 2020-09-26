29be2a5827315394dd58695d7f02c6bbb445893c
Holden Hardcastle <h0ld3n@four11productions.com>
2020-09-26 14:25:49 -0700

    Tending To My Digital Garden

--- includes/footer-social.php
+++ includes/footer-social.php
@@ -30,18 +30,21 @@
       							<ul>
                             		<li>Files Of Exile by Four 11 Productions &amp; Champagne Dreams</li>
                                     <li>Best Sound Design</li>
+                                    <li>&nbsp;</li>
                                     <li><i>"Outstanding achievement in the field of jargon...love the production value added with the graphics in the hacking section - great use of screen capture and the Hitchcock zoom looked awesome! I really liked the stream of consciousness computer jargon...love the ridiculous tech talk...NOTHING happened but I couldn't turn away!!!!! WOOOW incredible art design. Liked how the tech/coding was used as like an aesthetic accent that mirrored the tone of the short. The scrambled code overlaid with itself really added a sense of mania and confusion, which complimented my own fake-jargon-induced confusion quite nicely. Good stuff!! Super creative. Sound design is not to be slept on...I appreciated the film noir choices and the sparing use of color was impactful. The jumbled graphics and jumbled jargon of the script worked really well together...loved the character and world building established by very little - and awesome touches of humor especially at the end...I loved starting with the credits here. Classic Noir vibes. Really well-cut conversation and, as always, loved the jargon! I Really liked the stream of consciousness...The graphics are also good for such a short time, and I like the 40s feeling font....I liked that as she is so urgent the guy is still treating this weird intrusion as part of his day to day instead of being a big deal...the voice over lines up with Holden's face perfectly...smooth transitions, less jarring...liked the voiceover and subtlety of the performance. Dude going through mundane thoughts over and over made the interruption of the woman in the screen especially jarring...clean cuts and a good visual sense of humor...I thought the narrative of this edit was really easy to follow - the heist aspect of it all stood out as well as the choice to have it wholly in black and white which made it very noir. I loved the ticketing monologue at the end - very real to life to experiences with IT folks and honestly the writing is sharp - I feel like it's hard to make that much consistent rhythmic nonsense.</i></li>
                             	</ul>
                                 <li class="h4">Film Sprint Homebound Edition—Wild Card&nbsp;2020</li>
       							<ul>
                             		<li>Entry_3410 by Four 11 Productions</li>
                                     <li>Best Cinematography, Editing, Sound Design, &amp; Trailer Moment</li>
+                                    <li>&nbsp;</li>
                                     <li><i>"Loved the use of tape damage filters and the video distortion...really enjoyed it. Fun montage...loved the shot at the end where you couldn't force enjoyment. Kept the storytelling really tight and the whole project was really impressive...Excellent editing. Great, creative use of the space...Loved the world-building...Kudos for making something so watchable...The subject matter was so real, and it really struck a nerve. I loved the montage break from the tension...I really liked this...the shooting / cutting really gave a lot of tension...The video/audio distortion was really well done...this was solid."</i></li>
                             	</ul>
                             	<li class="h4">Film Sprint Homebound Edition—Documentary&nbsp;2020</li>
       							<ul>
                             		<li>Obscure Vinyl Boxset by Viral Media Network&nbsp;(VMN)</li>
                                     <li>Best Unheralded Film Of The Summer</li>
+                                    <li>&nbsp;</li>
                                 	<li><i>"...I loved everything about this. The graphics were perfect...cleverly edited...The use of stock footage and the choice to have a visualizer fill in when there was a VO moment with no footage was great..."</i></li>
                             	</ul>
                                 <li class="h4">American Graphic Design Awards&nbsp;2015</li>
@@ -80,23 +83,33 @@
     				</div>
                     <div class="col-md-2 col-md-offset-1">
                     	<p>
-      						<h3>Online Profiles</h3>
+      					<h3>Not Just Freelance</h3>	
                             <ul>
-                                <li><a href="https://about.me/holdenhardcastle" target="_blank"><span class="social social-rss"></span>about.me</a></li>
                                 <li><a href="https://angel.co/holdenhardcastle" target="_blank"><span class="social social-rss"></span>angel.co</a></li>
-                                <li><a href="https://www.behance.net/HoldenHardcastle" target="_blank"><span class="social social-behance"></span>behance</a></li>               <li><a href="http://cargocollective.com/holdenhardcastle" target="_blank"><span class="social social-rss"></span>cargo collective</a></li>
-                                <li><a href="http://holdenhardcastle.dunked.com/" target="_blank"><span class="social social-rss"></span>dunked</a></li>
-                                <li><a href="https://ello.co/h0ld3n" target="_blank"><span class="social social-rss"></span>ello</a></li>
-                                <li><a href="https://www.facebook.com/holden.hardcastle" target="_blank"><span class="social social-facebook"></span>facebook</a></li>
-                                <li><a href="https://github.com/h20ld3n" target="_blank"><span class="social social-github"></span>github</a></li>
-								<li><a href="http://en.gravatar.com/h20ld3n" target="_blank"><span class="social social-rss"></span>gravatar</a></li>
                                 <li><a href="https://hired.com/profile.holden-hardcastle" target="_blank"><span class="social social-rss"></span>hired</a></li>
-                                <li><a href="https://www.instagram.com/h20ld3n/" target="_blank"><span class="social social-instagram"></span>instagram</a></li>
                                 <li><a href="https://www.linkedin.com/in/holdenhardcastle" target="_blank"><span class="social social-linked-in"></span>linkedin</a></li>
                                 <li><a href="https://localsolo.com/san-francisco-ca/profile/16124/" target="_blank"><span class="social social-rss"></span>localsolo</a></li>
+                                <li><a href="https://www.patreon.com/holdenhardcastle" target="_blank"><span class="social social-rss"></span>patreon</a></li>
+
+                        </ul>
+                        <h3>Portfolios</h3>
+                            <ul>
+                                <li><a href="https://www.behance.net/HoldenHardcastle" target="_blank"><span class="social social-behance"></span>behance</a></li>               <li><a href="http://cargocollective.com/holdenhardcastle" target="_blank"><span class="social social-rss"></span>cargo collective</a></li>
+                                <li><a href="https://www.coroflot.com/HoldenHardcastle" target="_blank"><span class="social social-rss"></span>coroflot</a></li>
+                                <li><a href="http://holdenhardcastle.dunked.com/" target="_blank"><span class="social social-rss"></span>dunked</a></li>
+                                <li><a href="https://ello.co/h0ld3n" target="_blank"><span class="social social-rss"></span>ello</a></li>
+
+                        </ul>
+                        <h3>Social Networks</h3>
+                        <ul>
+                                <li><a href="https://about.me/holdenhardcastle" target="_blank"><span class="social social-rss"></span>about.me</a></li>
+                                <li><a href="https://www.facebook.com/holden.hardcastle" target="_blank"><span class="social social-facebook"></span>facebook</a></li>
+                                <li><a href="https://github.com/h20ld3n" target="_blank"><span class="social social-github"></span>github</a></li>
+                                <li><a href="https://www.goodreads.com/user/show/69130149-holden-hardcastle" target="_blank"><span class="social social-github"></span>goodreads</a></li>
+                                <li><a href="http://en.gravatar.com/h20ld3n" target="_blank"><span class="social social-rss"></span>gravatar</a></li>
+                                <li><a href="https://www.instagram.com/h20ld3n/" target="_blank"><span class="social social-instagram"></span>instagram</a></li>
                                 <li><a href="https://mstdn.social/web/accounts/214145" target="_blank"><span class="social social-twitter"></span>mastadon</a></li>           
                                 <li><a href="https://medium.com/@hhardcastle" target="_blank"><span class="social social-rss"></span>medium</a></li>
-                                <li><a href="https://www.patreon.com/holdenhardcastle" target="_blank"><span class="social social-rss"></span>patreon</a></li>
                                 <li><a href="https://www.pinterest.com/h20ld3n/" target="_blank"><span class="social social-pinterest"></span>pinterest</a></li>
                                 <li><a href="http://h0ld3n.tumblr.com/" target="_blank"><span class="social social-tumblr"></span>tumblr</a></li>
 								<li><a href="https://twitter.com/h20ld3n" target="_blank"><span class="social social-twitter"></span>twitter</a></li>
@@ -105,20 +118,31 @@
                             <ul>
 								<li><a href="https://blackmusiclibrary.com/" target="_blank"><strong>The Black Music History&nbsp;Library.</strong></a></li>
                                 <li>A living collection of books, articles, documentaries, series, podcasts and more about the Black origins of traditional and popular music dating from the 18th century to present day. Resources are organized chronologically and by genre for ease of browsing.</li>
-								<li><a href="https://deckofbrilliance.com/" target="_blank"><strong>Deck Of Brilliance</strong></a></li>
+								<li>&nbsp;</li>
+                              <li><a href="https://deckofbrilliance.com/" target="_blank"><strong>Deck Of Brilliance</strong></a></li>
                                 <li>Idea generation tools for creative professionals. By Juggi Ramakrishnan and Todd McCracken</li>
-                                <li><a href="https://designs.ai/" target="_blank"><strong>Designs.ai</strong></a></li>
+                                <li>&nbsp;</li>
+                              <li><a href="https://designs.ai/" target="_blank"><strong>Designs.ai</strong></a></li>
                                 <li>Part of a creative ecosystem on a mission to make design easy for everyone.</li>
-                                <li><a href="https://designmodo.com/" target="_blank"><strong>Designmodo</strong></a></li>
-                                <li><a href="http://flexboxfroggy.com/" target="_blank"><strong>Flexbox Froggy</strong></a></li>
+                                <li>&nbsp;</li>
+                              <li><a href="https://designmodo.com/" target="_blank"><strong>Designmodo</strong></a></li>
+                                <li>&nbsp;</li>
+                              <li><a href="http://flexboxfroggy.com/" target="_blank"><strong>Flexbox Froggy</strong></a></li>
                                 <li>A game where you help Froggy and friends by writing CSS code! <i>I am currently on level 8</i>.</li>
-                                <li><a href="https://mckups.com/" target="_blank"><strong>MCKUPS</strong></a></li>
+                                <li>&nbsp;</li>
+                              <li><a href="https://mckups.com/" target="_blank"><strong>MCKUPS</strong></a></li>
                                 <li>They have a lot of free mockups. Stay safe, stay well!</li>
-                                <li><a href="https://smartmockups.com/" target="_blank"><strong>Smartmockups</strong></a></li>
+                                <li>&nbsp;</li>
+                                <li><a href="https://roadmap.manager-tools.com/home" target="_blank"><strong>Roadmap Manager Tools</strong></a></li>
+                                <li>Weekly podcasts designed to give managers and individual contributors actionable recommendations...</li>
+                                <li>&nbsp;</li>
+                              <li><a href="https://smartmockups.com/" target="_blank"><strong>Smartmockups</strong></a></li>
                                 <li>Product Mockup Generator</li>
-                                <li><a href="https://www.peterjwagoner.com/starters-concepting-cards" target="_blank"><strong>Starter Concept Cards</strong></a></li>
+                                <li>&nbsp;</li>
+                              <li><a href="https://www.peterjwagoner.com/starters-concepting-cards" target="_blank"><strong>Starter Concept Cards</strong></a></li>
                                 <li>Peter J. Wagoner created a tool to help us come up with ideas. It's not a substitute for strategy or insight – it's a substitute for pulling another all nighter.</li>
-                                <li><a href="http://tevko.github.io/practice/" target="_blank"><strong>Tevko</strong></a></li>
+                                <li>&nbsp;</li>
+                              <li><a href="http://tevko.github.io/practice/" target="_blank"><strong>Tevko</strong></a></li>
                                 <li>Practice your skills by getting random suggestions or creative inspiration!</li>
                                 
                                 
