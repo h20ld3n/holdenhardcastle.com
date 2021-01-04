3be5b5d10b95e18ed14a529e8b737b96ff2ab2f9
Holden Hardcastle <h0ld3n@four11productions.com>
2021-01-04 14:55:49 -0800

    Removable Cartridges

---
+++ archive/removable-cartridges.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<?php include '../includes/meta.php';?>
+	<title>Holden Hardcastle's Removable Cartridges</title>
+	<?php include '../includes/link.php';?>
+</head>
+<body>
+	<h1 style="display:none;">Holden Hardcastle</h1>
+	<?php include '../includes/navbar.php';?>
+	<header>
+		<div class="jumbotron" id="holden-hardcastle">
+			<div class="container">
+    			<div class="row">
+					<div class="col-md-6 col-md-offset-3">
+        				<h1>Removable Cartridges &amp; Disks</h1>
+        				<h2>A hard disk enclosed in a case that can be removed from the disk drive, having more storage than floppy disks.</h2>
+                   		<p>Alternatively referred to as removable storage or removable media, a removable disk is media that enables a user to move data between computers without having to open their computer.</p>
+  					</div>
+                </div>
+ 			</div>
+		</div>
+	</header>
+	<main>
+		<section>
+		<h2>Hotel California</h2>
+			<article>
+    		<div class="jumbotron" id="design-issues">
+      			<div class="container-fluid">
+        			<div class="row">
+                    	<img src="../images/design-issues/200514-design-issues.png" alt="random pages from my sketchbook" class="img-responsive">
+                    </div>
+      			</div>
+    		</div>
+    		<div class="container">
+      			<div class="row">
+        			<div class="col-md-6 col-md-offset-3" id="description">
+          				<h3>Color structure valuation is taught through the color wheel.</h3>
+						<p> The primary colors, yellow, red, blue, and secondary colors, orange, green, violet, are invariably included in such a chart. But it usually also includes expanded mixtures of at least twelve hues. From this simple chart of a color wheel, multiple variations of color hues can be developed, which may or may not suggest to each other under certain circumstances. The judgement of the eye is usually checked on by the hand by actual touching. Is it really smooth, or does it just look that way? Is that an indentation or a raised mark? No wonder there are so many "DO NOT TOUCH!" signs. </p>
+                    </div>
+      			</div>
+    		</div>
+  		</article>
+		</section>
+    <?php include '../includes/footer-social.php';?>
+</main>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="../bootstrap/js/jquery-1.11.3.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<!-- script src="js/bootstrap.min.js"></script-->
+</body>
+</html>
