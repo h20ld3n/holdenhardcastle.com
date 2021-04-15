d95440be7e617b7665c2cb0328d0b811925365fc
Holden Hardcastle <h0ld3n@four11productions.com>
2021-04-15 16:28:57 -0700

    Self Portrait: A La Man #1 & #2

---
+++ archive/kirt-hardcastle-paintings.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<?php include '../includes/meta.php';?>
+	<title>Kirt Hardcastle</title>
+	<?php include '../includes/link.php';?>
+</head>
+<body>
+	<h1 style="display:none;">Kirt Hardcastle</h1>
+	<?php include '../includes/navbar.php';?>
+	<header>
+		<div class="jumbotron" id="holden-hardcastle">
+			<div class="container">
+    			<div class="row">
+					<div class="col-md-6 col-md-offset-3">
+        				<h1>Kirt Hardcastle (1949 - 2018)</h1>
+        				<h2>Artist, Painter, Philosopher</h2>
+                   		<!--p>(2016) Collaborated with clients and project leads to determine opportunities, goals, style, and tone for media campaigns. Communicated a vision and strategy to designers, illustrators, copywriters, and other artists. Approved artwork and delivered client presentations to review and finalize products.</p-->
+  					</div>
+                </div>
+ 			</div>
+		</div>
+	</header>
+	<main>
+	<section>
+  		<h2>Dell EMC Hybrid Cloud and VCE Customer Value Infographics</h2>
+  		<article>
+    		<div class="jumbotron" id="bleu-blue">
+      			<div class="container-fluid">
+        			<div class="row">
+                    	<img src="../images/kirt-hardcastle/475-477-self-portrait-a-la-man.png" alt="self portrait: a la man #1 & #2" class="img-responsive">
+                    </div>
+      			</div>
+    		</div>
+    		<div class="container">
+      			<div class="row">
+        			<div class="col-md-6 col-md-offset-3" id="description">
+          				<h3>Self Portrait: A La Man &#35;1 &amp; &#35;2</h3>
+                    </div>
+      			</div>
+    		</div>
+  		</article>
+	</section>
+    <?php include '../includes/footer-social.php';?>
+</main>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
+<script src="bootstrap/js/jquery-1.11.3.min.js"></script> 
+<script src="https://code.jquery.com/jquery-2.2.4.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.2.2/Chart.bundle.js"></script>
+<script  src="js/chart.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+</body>
+</html>
