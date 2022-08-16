06c6f630d55d1ba0105301847215fe82beb0a28a
Holden Hardcastle <h0ld3n@four11productions.com>
2022-08-16 08:28:57 -0700

    Ready State Portfolio Updates — 0816

--- archive/ready-state.php
+++ archive/ready-state.php
@@ -13,8 +13,8 @@
     			<div class="row">
 					<div class="col-md-6 col-md-offset-3">
         				<h1>Ready State</h1>
-        				<h2>Senior Designer—Amazon</h2>
-                   		<p>(2016-2020) A brand steward who directed designers, production artists, copywriters, and other specialists to ensure consistency for 360 marketing campaigns. Delivered collateral and assets across channels and mediums on schedule and to specifications.</p>
+        				<h2>Senior Designer</h2>
+                   		<p>(2021-2022) Worked with Amazon, Instacart, Markforged, Snowflake, and Torani to develop a variety of media from concept to final execution. Responsible for editing video and creating strategic content for blogs, social media, and marketing materials.</p>
   					</div>
                 </div>
  			</div>
@@ -22,150 +22,94 @@
 	</header>
 	<main>
 	<section>
-  		<h2>Williams Lea Tag &amp; Union Bank</h2>
 		<article>
-    		<div class="jumbotron" id="ub-cool-grey3">
+    		<div class="jumbotron" id="readystate-cream">
       			<div class="container-fluid">
-        			<div class="row">
-                    	<img src="../images/tag-ub-email-design-specs.png" alt="Union Bank Email Design and Specs" class="img-responsive">
+					 <div class="col-md-6">
+                        <img src="../images/rs-amazon-firetv-get-to-watching.webp" alt="Amazon FireTV Feature Rotator" style="width:100%"> <!-- class="img-responsive"-->
+                    </div>
+                    <div class="col-md-6">
+                        <img src="../images/rs-amazon-firetv-simplify.webp" alt="Amazon FireTV Feature Rotator" style="width:100%"> <!-- class="img-responsive"-->
                     </div>
       			</div>
     		</div>
     		<div class="container">
       			<div class="row">
         			<div class="col-md-6 col-md-offset-3" id="description">
-          				<h3>Email Design Updates</h3>
-						<p>Worked with Union Bank channel and marketing leads, brand managers, Salesforce development leads, and designers to develop email mock-ups, redlines, templates, and specs that adhered to UX / UI standards, accessibility, and corporate guidelines.</p>
+          				<h3>Amazon FireTV</h3>
+						<p>Feature rotators &amp; blog heroes</p>
 					</div>
 					<div class="col-md-12">
-						<img src="../images/tag-ub-email-specs.png" class="img-responsive">
-					</div>
-					<div class="col-md-6 col-md-offset-3" id="description">
-          				<p>The established templates resulted in a 75% reduction in the time required to deliver final assets. The increase in efficiency of asset delivery allowed for strategic and design thinking around email campaigns.</p>
-                    </div>
-      			</div>
-    		</div>
-  		</article>
-		<article>
-    		<div class="jumbotron" id="ub-cool-grey3">
-      			<div class="container-fluid">
-        			<div class="row">
-                    	<img src="../images/tag-ub-lunar-new-year.webp" alt="Lunar New Year Landing Page" class="img-responsive">
-                    </div>
-      			</div>
-    		</div>
-    		<div class="container">
-      			<div class="row">
-        			<div class="col-md-6 col-md-offset-3" id="description">
-          				<h3>Lunar New Year Landing Page</h3>
-						<p>With deep ties to Eastern cultures and traditions, the landing page and additional assets were designed and developed to raise awareness and goodwill for the MUFG and Union Bank brands.</p>
+						<img src="../images/rs-amazon-blog-heros.webp" alt="Amazon FireTV Blog Heros" style="width:100%"> <!-- class="img-responsive"-->
 					</div>
       			</div>
     		</div>
   		</article>
 		<article>
-    		<div class="jumbotron" id="ub-cool-grey3">
+    		<div class="jumbotron" id="readystate-cream">
       			<div class="container-fluid">
-        			<div class="row">
-                    	<img src="../images/tag-ub-home-lending-2019.png" alt="Union Bank Email Design and Specs" class="img-responsive">
-                    </div>
+					 <div class="col-md-12">
+						<img src="../images/rs-markforged-art-part.webp" alt="Art to Part White Paper" style="width:100%"> <!-- class="img-responsive"-->
+					</div>
       			</div>
     		</div>
     		<div class="container">
       			<div class="row">
         			<div class="col-md-6 col-md-offset-3" id="description">
-          				<h3>Home Lending Campaign</h3>
-						<p>The nine-month 360 multi-channel marketing campaigns included printed collateral, ATMs, digital signage, landing pages, emails, and in-branch environmental signage featured across all Union Bank branches. The goal was to drive conversations of mortgage pre-qualification and raise awareness of the advantages of using your home's equity, which in turn should generate increased application volumes from interested customers.</p>
+          				<h3>Markforged</h3>
+						<p>E-books, white papers, infographics..</p>
 					</div>
 					<div class="col-md-12">
-						<img src="../images/tag-ub-mortgage-360.png" class="img-responsive">
+						<img src="../images/rs-markforged-one-pager.webp" alt="one pagers" style="width:100%"> <!-- class="img-responsive"-->
 					</div>
-					<div class="col-md-6 col-md-offset-3" id="description">
-                        <p><i>Background &amp; audience:</i> The "purchase season" or prime time for both mortgage and home equity lines of credit (HELOC) is the spring & summer months. During the spring and summer months, when the weather is beautiful, many families begin a home a new home search in the hopes of moving or buying a home before the next school year. Homeowners are also racking up record amounts of home equity, thanks to fast-rising values in today's competitive housing market, and more people are now starting to tap into that cash.</p>
-                    </div>
-                    <div class="col-md-12">
-						<img src="../images/tag-ub-heloc-360.png" class="img-responsive">
+      			</div>
+    		</div>
+			<div class="container">
+      			<div class="row">
+					<div class="col-md-12">
+						<img src="../images/rs-markforged-ebook-aerospace-01.webp" alt="Aerospace ebook" style="width:100%"> <!-- class="img-responsive"-->
 					</div>
-					<div class="col-md-6 col-md-offset-3" id="description">
-                        <p><i>Market and competitive considerations:</i> Interest rates are the number one driver of when to get a home equity line of credit and mortgage. In late 2019 and early 2020, mortgage rates were at their lowest since late 2016 and near historic lows, making it an ideal time to buy or refinance a home. Refinance applications were up 116% compared with the previous year. Low mortgage rates were fueling a housing shortage, and the number of homes for sale nationwide fell 2.5% from the prior year. The drop in inventory was prompted by low mortgage rates and led to fast-paced sales and higher home prices.</p>
-                        <p>The Fed lowered interest rates in September 2019, and borrower home equity rose to an all-time high in the first half of 2019. Homeowners with a mortgage—about 63% of all homeowners—saw equity increase by 4.8%, a total of nearly $428 billion, since the second quarter of the previous year. The average homeowner gained about $4,900 in equity over the last quarter.</p>
-                    </div>
+      			</div>
+    		</div>
+			<div class="container">
+      			<div class="row">
+					<div class="col-md-12">
+						<img src="../images/rs-markforged-ebook-aerospace-02.webp" alt="Aerospace ebook" style="width:100%"> <!-- class="img-responsive"-->
+					</div>
       			</div>
     		</div>
   		</article>
-		
-		<article>
-    		<div class="jumbotron" id="ub-cool-grey3">
+				<article>
+    		<div class="jumbotron" id="readystate-cream">
       			<div class="container-fluid">
-        			<div class="row">
-                    	<img src="../images/wlt-ub-small-business-promo.png" alt="Small Business Campaign" class="img-responsive">
-                    </div>
+					 <div class="col-md-12">
+						<img src="../images/rs-snowflake-tier-01.webp" alt="Tier 1" style="width:100%"> <!-- class="img-responsive"-->
+					</div>
       			</div>
     		</div>
     		<div class="container">
       			<div class="row">
         			<div class="col-md-6 col-md-offset-3" id="description">
-          				<h3>Small Business and Merch. Campaigns</h3>
-          				<p>MUFG Union Bank aims to be a foundation of strength committed to meeting the needs of its customers, serving society, and fostering shared and sustainable growth for a better world.</p>
-                        <p>Working with this mission in mind, the small business out of home advertising grew to become the first of many 360-marketing campaigns that included digital and ATM screens, emails, landing pages, social media posts, to name a few.</p>
-                    </div>
+          				<h3>Snowflake</h3>
+						<p>Blog hero images and social media</p>
+					</div>
+					<div class="col-md-12">
+						<img src="../images/rs-snowflake-tier-02.webp" alt="Tier 2" style="width:100%"> <!-- class="img-responsive"-->
+					</div>
       			</div>
     		</div>
-  		</article>
-		<article>
-    		<div class="jumbotron" id="ub-cool-grey3">
-      			<div class="container-fluid">
-        			<div class="row">
-                    	<img src="../images/wlt-ub-credit-cards.png" alt="Union Bank Credit Cards" class="img-responsive">
-                    </div>
-      			</div>
-    		</div>
-    		<div class="container">
+			<div class="container">
       			<div class="row">
-        			<div class="col-md-6 col-md-offset-3" id="description">
-          				<h3>Union Bank Credit &amp; ATM&nbsp;Cards</h3>
-          				<p>Williams Lea Tag was tasked with creating "top of wallet" campaigns and collateral that aligned with the bank's mission. Working closely with team and channel leads—credit and ATM cards, materials, and strategies were developed to retain, update, and grow the Union Bank customer base.</p>
-                    </div>
+					<div class="col-md-12">
+						<img src="../images/rs-snowflake-tier-03.webp" alt="Tier 3" style="width:100%"> <!-- class="img-responsive"-->
+					</div>
       			</div>
     		</div>
-  		</article>
-		<article>
-    		<div class="jumbotron" id="ub-cool-grey3">
-      			<div class="container-fluid">
-        			<div class="row">
-                    	<img src="../images/wlt-ub-car-wrap.png" alt="Union Bank Car Wrap" class="img-responsive">
-                    </div>
-      			</div>
-    		</div>
-    		<div class="container">
+			<div class="container">
       			<div class="row">
-        			<div class="col-md-6 col-md-offset-3" id="description">
-          				<h3>Union Bank Car&nbsp;Wrap</h3>
-          				<dl>
-  						<dd>
-        				<canvas id="canvas" height="450" width="600"></canvas>
-  						</dd>
-						</dl>
-						<p>By identifying target audiences, crafting direct messaging, and adhering to brand guidelines, cohesive campaigns were developed that resulted in a 400% increase in customer response rates.</p>
-                    </div>
-      			</div>
-    		</div>
-  		</article>
-		<article>
-    		<div class="jumbotron" id="ub-cool-grey11">
-      			<div class="container-fluid">
-        			<div class="row">
-                    	<img src="../images/wlt-ub-onserts.png" alt="Onserts and direct mail" class="img-responsive">
-                    </div>
-      			</div>
-    		</div>
-    		<div class="container">
-      			<div class="row">
-        			<div class="col-md-6 col-md-offset-3" id="description">
-          				<h3>Onserts and direct&nbsp;mail</h3>
-          				<p>Collateral created was delivered with monthly statements and placed in branches. These pieces would eventually lead to an increased response rate and would be the templates for future campaigns and brand guidelines. </p>
-						<p><a class="btn btn-default" href="../index.php#Union-Bank" role="button" style="margin-top: 1em; width: 100%; font-size: .75em;">Back to main page</a></p>
-                    </div>
+					<div class="col-md-12">
+						<img src="../images/rs-snowflake-tier-04.webp" alt="Tier 4" style="width:100%"> <!-- class="img-responsive"-->
+					</div>
       			</div>
     		</div>
   		</article>
