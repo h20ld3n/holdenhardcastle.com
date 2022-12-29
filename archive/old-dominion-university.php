46fe07b3293494ff3fd5a235bd74b78813992c02
Holden Hardcastle <h0ld3n@four11productions.com>
2022-12-29 15:33:31 -0800

    imgIX deployment

--- archive/old-dominion-university.php
+++ archive/old-dominion-university.php
@@ -1,48 +1,9 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-	<meta name="description" content="Aspiring to create beautiful, exciting, and fun experiences by collaborating and working with inspired and driven companies, teams, and people.">
-	<meta name="keywords" content="HTML,CSS,XML,JavaScript">
-	<meta name="author" content="Holden Hardcastle">
-	<meta http-equiv="Cache-Control" content="max-age=200" />
-    <meta name="pinterest" content="nopin" />
-    <meta name="theme-color" content="#ffffff">
-	<title>Holden Hardcastle</title>
-	<!--script>!function(a,b,c,d,e){function g(a,c,d,e){var f=b.getElementsByTagName("script")[0];a.src=e,a.id=c,a.setAttribute("class",d),f.parentNode.insertBefore(a,f)}a.Mobify={points:[+new Date]};var f=/((; )|#|&|^)mobify=(\d)/.exec(location.hash+"; "+b.cookie);if(f&&f[3]){if(!+f[3])return}else if(!c())return;b.write('<plaintext style="display:none">'),setTimeout(function(){var c=a.Mobify=a.Mobify||{};c.capturing=!0;var f=b.createElement("script"),h="mobify",i=function(){var c=new Date;c.setTime(c.getTime()+3e5),b.cookie="mobify=0; expires="+c.toGMTString()+"; path=/",a.location=a.location.href};f.onload=function(){if(e)if("string"==typeof e){var c=b.createElement("script");c.onerror=i,g(c,"main-executable",h,mainUrl)}else a.Mobify.mainExecutable=e.toString(),e()},f.onerror=i,g(f,"mobify-js",h,d)})}(window,document,function(){a=/webkit|(firefox)[\/\s](\d+)|(opera)[\s\S]*version[\/\s](\d+)|(trident)[\/\s](\d+)/i.exec(navigator.userAgent);return!a||a[1]&&4>+a[2]||a[3]&&11>+a[4]||a[5]&&6>+a[6]?!1:!0},
-
-	// path to mobify.js
-	"//cdn.mobify.com/mobifyjs/build/mobify-2.0.16.min.js",
-
-	// calls to APIs go here (or path to a main.js)
-	function() {
-	 		var capturing = window.Mobify && window.Mobify.capturing || false;
-
-	  		if (capturing) {
-    		Mobify.Capture.init(function(capture){
-      		var capturedDoc = capture.capturedDoc;
-
-      		var images = capturedDoc.querySelectorAll("img, picture");
-      		Mobify.ResizeImages.resize(images);
-        
-      		// Render source DOM to document
-      		capture.renderCapturedDoc();
-    		});
-  		}
-	});</script-->
-    <link href="https://fonts.googleapis.com/css?family=Abhaya+Libre" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="../bootstrap-3.3.6-dist/css/bootstrap.min.css">
-	<link rel="stylesheet" href="../css/glyphicons-social.css">
-	<link rel="stylesheet" href="../css/glyphicons-filetypes.css">
-    <link rel="stylesheet" type="text/css" href="../css/golden-ratio-1.618.css">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-	<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-	<link rel="manifest" href="/manifest.json">
-	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-	
+	<?php include '../includes/meta.php';?>
+	<title>Old Dominion University — College Portfolio & Ramblings</title>
+	<?php include '../includes/link.php';?>
 </head>
 <body>
 	<h1 style="display:none;">Old Dominion University</h1>
@@ -74,7 +35,6 @@
     		</div>
     		<div class="container">
       			<div class="row">
-                	<img src="../images/transparency.png"  alt="The ODU Theatre of the Absurd" class="img-responsive">
         			<div class="col-md-6 col-md-offset-3" id="description">
           				<h3>Theatre of the&nbsp;Absurd</h3>
           				<p>While working on "the absurd posters", discussions about the college student audience, lifestyle, and sexuality where rampant. The cyclical, repetitive, and often meaningless activities of the undergrad defined the monkey spanking the monkey image. The monkey image was the first of a series of illustrations created specifically for the posters. Later ideas of scale, cliche, and dated icons surfaced and defined the look and feel of the posters.</p>
@@ -92,7 +52,6 @@
     		</div>
     		<div class="container">
       			<div class="row">
-                	<img src="../images/transparency.png"  alt="Virginia Air And Space Posters" class="img-responsive">
         			<div class="col-md-6 col-md-offset-3" id="description">
           				<h3>Business Cards &amp; Letterhead</h3>
         				<p>My professor wasn't crazy about this treatment for the children's optometrist letterhead, envelopes and business cards. It wasn't until later that I was ok with that.</p></div>
@@ -110,7 +69,6 @@
     		</div>
     		<div class="container">
       			<div class="row">
-                	<img src="../images/transparency.png"  alt="Virginia Air And Space Posters" class="img-responsive">
         			<div class="col-md-6 col-md-offset-3" id="description">
           				<h3>Virginia Air &amp; Space Posters</h3>
         				<p>Attik Designs heavily influenced the work created when the Virginia Air and Space Center asked me to create some cool educational posters, that kids would want to pin up in their rooms. The posters had to live next to supermodels, cars, magazine pullouts and rock stars. The strong imagery of the Space Shuttle launch and jets flying straight on center were enhanced with illustrations and interpretations of aerodynamic patterns.</p></div>
