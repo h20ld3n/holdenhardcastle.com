c5c90ba8072b1724e534521f60ba44510abb0843
Holden Hardcastle <h0ld3n@four11productions.com>
2023-02-08 12:33:08 -0800

    Burden of Loving v.03.08

---
+++ archive/burden-of-loving.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<?php include '../includes/meta.php';?>
+	<title>Soaring — A Literary Magazine</title>
+	<?php include '../includes/link.php';?>
+</head>
+<body>
+	<h1 style="display:none;">Art Director — Tag — Union Bank</h1>
+	<?php include '../includes/navbar.php';?>
+	<header>
+		<div class="jumbotron" id="holden-hardcastle">
+			<div class="container">
+    			<div class="row">
+					<div class="col-md-6 col-md-offset-3">
+        				<h1>Soaring</h1>
+        				<h2>A Literary Magazine</h2>
+                   		<p>(1991) <strong>Dedication</strong> This isssue of Soaring is dedicated with gratitude to the memory of John Lee, former head of the reprographics department. Without his commitment and painstaking work, last year's issue would not have been possible. He would always say, "The students should have this magazine."</p>
+  					</div>
+                </div>
+ 			</div>
+		</div>
+	</header>
+	<main>
+	<section>
+  		<h2>The Burden of Loving</h2>
+		<article>
+    		<div class="jumbotron" id="ub-cool-grey3">
+      			<div class="container-fluid">
+        			<div class="row">
+                    	<img src="https://holdenhardcastle.imgix.net/tag-ub-email-design-specs.png" alt="Union Bank Email Design and Specs" class="img-responsive">
+                    </div>
+      			</div>
+    		</div>
+    		<div class="container">
+      			<div class="row">
+        			<div class="col-md-6 col-md-offset-3" id="description">
+          				<h3>by Lorraine Hardcastle</h3>
+						<p>"Men are idiots," Marni thought to herself as she pulled into the driveway. She could feel her resentment gorwing as she eased in next to his truck.</p>
+                        <p>"You would be here," she said out loud. As she reached the door a pain shot through her abdomen and doubled over, gripping the door knob for support. The momentary agony passed and she walked inside.</p>
+                        <p>Marni closed the door behind her, shutting out the bright sunlight. She took off her sunglasses, put them in her purse, and stood silently in the hallway waiting for her eyes to adjust to the darkness. An involuntary shiver seized her as she moved slowly towards the overstuffed rocking chair in the living room. The cold of the room made her feel as though she had stepped into a meat locker. Gen was sleeping on the couch, television blaring, curtains drawn. She turned down the volume as she passed, picked up the blanket on the floor, wrapped her self in it, and gingerly settled in the rocker, sighing deeping as she sank down.</p>
+                        <p>She counted the beer bottles on the coffee table. Eight of them. "That guarantees me an hour or two of peace," she quitely to herself. Again the pain overwhelmed her and Marni reach for the Quaaludes in her purse. She picked up a warm bee from the table and swallowed two tablets. Easing back into the chair she waited for the familiar numbing effect. Her mind kept repeating a line from an old Beatles song, and sang it to herseld in the hop of exorcising it.</p>
+                        <p>"I know what it's like to be dead," she repeated several times. "Boy, is that the truth."</p>
+                        <p>She shook her head and started to laugh, but remembering Gene on the couch cut her off. "What am I doing here?" As the pill took effect her mind wandered back to the day they'd met.</p>
+                        <p>Stevens Sport Parachuting in Antioch. Who'd talked her into that? A frown crossed her face as she ran through a mental list of men. The answer refused to come to her. What did it matter? The disc jockey, the bartender, the highway patrolamn; it was all the same. WHoever he was, their relationship had ended that day.</p>
+                        <p>She'd been pretty loaded on speed but managed to pass the pre-jump ground training with a minimum of bruises. As she stood waiting by the plane he walked up behind her and put his hand on her shoulder. "I'm Gene. I'll be your jumpmaster You're not nervous are you? Don't waorry, I've never bounced a student yet," he said grinning widely.</p>
+                        <p>Marni was so frightened she could speak, so she nodded as he kept up his reassuring banter. He was gorgeous, a true Adonis. Six foot four, blonde hair, the bluest eyes she had ever seen, tanned, muscular, not an inch of fat anywhere, and she was looking carefully. Obviously the outdoor type, quiet southern drawl, a man's man.</p>
+                    </div>
+      			</div>
+    		</div>
+  		</article>
+	</section>
+    <?php include '../includes/footer-social.php';?>
+</main>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
+<script src="bootstrap/js/jquery-1.11.3.min.js"></script> 
+<script src="https://code.jquery.com/jquery-2.2.4.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.2.2/Chart.bundle.js"></script>
+<script  src="../js/chart.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+</body>
+</html>
